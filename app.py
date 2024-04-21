9a514321267597b665c3d9ed43d81d8f48e4740b
AmanS <sharma00123aman@gmail.com>
2024-04-21 22:27:50 +0530

    added multiply file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from calc_func import do_addition,do_subtraction
+from multiply import do_multiply
 def main():
     print('welcome to the calculator App')
     print("""
           \n Selectthe function from the give options
           1.Add
           2.subtration
+          3.multiply
           """)
 if __name__=="__main__":
     main()
@@ -17,6 +19,8 @@ if user_input=="1":
     result=do_addition(a,b)
 elif user_input=="2":
     result=do_subtraction(a,b)
+elif user_input=="3":
+    result=do_multiply(a,b)
 
 print('Result :',result)
 
