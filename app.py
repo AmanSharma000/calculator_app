b516e68b7596e933301f7dde3ffdaff03bd6ae9e
AmanS <sharma00123aman@gmail.com>
2024-04-23 21:29:25 +0530

    division commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from calc_func import do_addition,do_subtraction
+from calc_func import do_addition,do_subtraction,do_division
 from multiply import do_multiply
 def main():
     print('welcome to the calculator App')
@@ -7,6 +7,7 @@ def main():
           1.Add
           2.subtration
           3.multiply
+          4.Division
           """)
 if __name__=="__main__":
     main()
@@ -21,6 +22,8 @@ elif user_input=="2":
     result=do_subtraction(a,b)
 elif user_input=="3":
     result=do_multiply(a,b)
+elif user_input=="4":
+    result=do_division(a,b)    
 
 print('Result :',result)
 
